0d09d62f722663625ccb14b8bb7d0d1a8a42a544
Colin Nykanen <colin@destash.io>
2023-11-07 19:34:40 -0700

    added CypherMessage class

---
+++ src/CypherMessage.java
@@ -0,0 +1,20 @@
+public class CypherMessage {
+    private String key;
+    private String message;
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public void setKey(String key){
+        this.key = key;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        String key = scnr.nextLine();
+        String message = scnr.nextLine();
+
+        CypherMessage cypherMessage = new CypherMessage();
+        cypherMessage.setKey(key);
+        cypherMessage.setMessage(message);
+
+        System.out.printf("key: %s%n", cypherMessage.getKey());
+        System.out.printf("message: %s%n", cypherMessage.getMessage());
     }
 }
