901c9133a3d790a7f147e7c07a68125fe71e0145
Griffin <griffinmanninen56@gmail.com>
2023-11-12 17:46:51 -0800

    updated 11/12/2023

--- src/UserPrompt.java
+++ src/UserPrompt.java
@@ -1,5 +1,23 @@
+import java.util.Scanner
+
+public class UserPrompt() {
 
 
-public class UserPrompt {
+   public static void main(String[] args){
+   Scanner userInput = new Scanner(System.in)
+
+   System.out.println("Input phrase to be altered: ");
+   String userMessage = userInput.nextLine();
+
+   System.out.println("Imput cypher key: ");
+   String userKey = userInput.nextLine();
+
+
+   cypherMessage cm = new cycpherMessage;
+   cm.setKey(userKey);
+   cm.setMessage(userMessage);
+   
+
+}
 
 }
