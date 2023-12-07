f0874a5b46b65dba33e3cff12577045e2f5a990a
Griffin <griffinmanninen56@gmail.com>
2023-12-06 21:17:55 -0700

    cleaned up, added comments

--- src/EncodeDecode/Algorithm.java
+++ src/EncodeDecode/Algorithm.java
@@ -1,82 +1,65 @@
-package EncodeDecode;
+package EncodeDecode; //states this class is bundled with other classes in the EncodeDecode folder
 
-import java.util.Random;
-import java.util.Scanner; 
+import java.util.Random; //imports java's random class used to create an obj, which is used here for creating random values in a repeatable manner using a seed
 
-public class Algorithm {
+public class Algorithm {//states class header
 
-    public String algorithmize(CypherMessage cypherMessage, String action) {
-        
-        if (action.equals(cypherMessage.ACTION_ENCODE)) {
-            System.out.println("ENCODE Algorithmize");
-         
-         //encode algorithm process starts here, edited 11/19/2023 - GM
-         int i;
-         String keyString = cypherMessage.getKey();
-         int key = Integer.parseInt(keyString);
-         char[] cArray = cypherMessage.getMessage().toCharArray(); //convert message to char array
-         
-         int range = 94; //32 to 126 ascii range, wont throw errors or weird characters;
+    final int range = 94; // this is used for the randomizer object to generate a random value only from 0-94, so values don't shift past the range of ASCII characters being used.
+                          //using 32 to 126 ascii range, wont throw errors or weird characters;
+                          
+    public String algorithmize(CypherMessage cypherMessage, String action) { //states algorithmize method, returns either a encoded/decoded string from two string inputs message and action
+    
+         int i; //used in for loop indexing
+         int key = Integer.parseInt( cypherMessage.getKey() ); //parses key as integer from the user inputted string
+         char[] charArray = cypherMessage.getMessage().toCharArray(); //convert message to char array
          int rand; //this will be the integer created from the random key used to adjust the ascii values
-         Random randomize = new Random();
-         randomize.setSeed(key); //sets random pattern with key value
-         rand =  (int)(randomize.nextInt(range));
-         // System.out.println(rand); debug line
          
-         //encode for-loop, the "magic" process of jumbling the message
-         for(i=0; i < cArray.length ;i++){
-         //System.out.println(cArray[i] + " to ");
-            if((cArray[i] + rand) > 126){
-               cArray[i] = (char) ((cArray[i] + rand) - 95);
-               //System.out.println( cArray[i]);
-               }
+         Random randomize = new Random(); //creates obj from Java's random
+         randomize.setSeed(key); //sets a distinct random numerical pattern with key value
+         rand = (int)(randomize.nextInt(range)); //finds next integer in the numerical pattern stored in randomize object and assigns it to variable "rand", used further down
+         
+        
+        
+        if (action.equals(cypherMessage.ACTION_ENCODE)) { //if statement for when user had inputted encode
+            System.out.println("ENCODE Algorithmize"); //lets user know encode has triggered
+         
+         //encode for loop algorithm process starts here, this shifts the ascii value then re-assigns it back into the same position in the array
+         for(i=0; i < charArray.length ;i++){ //for loop runs from the 0 to end, adjusts end to length of message (charArray). increments i by 1 each time
+         
+            if((charArray[i] + rand) > 126){ //for chars exceeding ascii range with rand value. 
+               charArray[i] = (char) ((charArray[i] + rand) - 95);} //the values of the characters exceeding 126 shift up the remaining amount from the bottom of the range
+              
             else{
-            
-            cArray[i] = (char) (cArray[i] + rand);
-            //System.out.println( cArray[i]);
-            }
+            charArray[i] = (char) (charArray[i] + rand);}//if the value dont exceed top of ascii range, they simply shift up by random generated value
    
          }
-         
          //algorithm jumbling process ends here
          
-         //convert char array back to string
-         String str = String.copyValueOf(cArray);
-         cypherMessage.setMessage(str); //str string is thrown to setMessage in cypherMessage class and replaces former string that was just altered.
+         String encodeStr = String.copyValueOf(charArray); //takes the array of characters that was altered and puts them back into string format.
+         cypherMessage.setMessage(encodeStr); //encoded string is thrown to setMessage in cypherMessage class,replacing the string that was just altered.
         }
 
-        if (action.equals(cypherMessage.ACTION_DECODE)) { //Process here for decoding will work similarly as above, just in the opposite order. edited 12/6/23 - GM     
-         int j;
-         String keyString = cypherMessage.getKey();
-         int key = Integer.parseInt(keyString);
-         char[] cArray = cypherMessage.getMessage().toCharArray(); //convert message to char array
-         
-         int range = 94; //for 32 to 126 ascii range, wont throw errors or weird characters;
-         int rand; //this will be the integer created from the random key used to adjust the ascii values
-         Random randomize = new Random();
-         randomize.setSeed(key); //sets random pattern with key value
-         rand = (int)(randomize.nextInt(range));
-         //System.out.println(rand); debug line
 
+
+        if (action.equals(cypherMessage.ACTION_DECODE)) { //Process here for decoding will work similarly as above, just in the opposite order. Triggers when user inputs decode 
+
+         //same jumbling process but the if/else algorithm is altered to undo the jumbling back into the original values so long the same key is inputted
+         for(i=0; i < charArray.length ;i++){
          
-         for(j=0; j < cArray.length ;j++){
-         //System.out.println( cArray[j] + " to ");
-            if((cArray[j] - rand) < 32){ //for chars exceeding ascii range with shifted amount
-               cArray[j] = (char) ((cArray[j] + 95) - rand);
-              // System.out.println( cArray[j]);
-              }
-            else{
-            cArray[j] = (char) (cArray[j] - rand);
-            //System.out.println( cArray[j]);
-            }
+            if((charArray[i] - rand) < 32){ //for the chars that had previosuly exceeded ascii range with rand value. 
+            charArray[i] = (char) ((charArray[i] + 95) - rand);} //the values shift back down from the top of the range using the remaining amount
             
-         String str = String.copyValueOf(cArray);
-         cypherMessage.setMessage(str);
+            else{
+            charArray[i] = (char) (charArray[i] - rand);} //shifts the other characters back down by the random generated value
+            
+         String decodeStr = String.copyValueOf(charArray); //takes the array of characters that was altered and puts them back into string format.
+         cypherMessage.setMessage(decodeStr); //decoded string is thrown to setMessage in cypherMessage class,replacing the string that was just altered.
  
          }
             
         }
 
-        return cypherMessage.getMessage();
+
+        return cypherMessage.getMessage(); //throws back either encoded or decoded string after the corresponding if statement triggers
     }
 }
